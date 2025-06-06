32d89e951c7a245436ce13538a8c4b3cbef406a7
jaimergp <jaimergp@users.noreply.github.com>
2025-06-06 12:47:43 +0200

    parameterize sys.prefix

--- tests/test_pep725.py
+++ tests/test_pep725.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2025 The meson-python developers
 #
 # SPDX-License-Identifier: MIT
+import os
 import subprocess
 import sys
 
@@ -13,15 +14,20 @@ import mesonpy
 from .conftest import in_git_repo_context
 
 
+CONDA_PREFIX = os.environ.get("CONDA_PREFIX", os.environ.get("CONDA", sys.prefix))
+
+
 def _install_external(directory: Path, ecosystem: str = "conda-forge") -> subprocess.CompletedProcess:
     external = External.from_pyproject_path(directory / "pyproject.toml")
     cmd = external.install_command(ecosystem=ecosystem, package_manager="conda")
-    return subprocess.run(cmd, check=True)
+    env = os.environ.copy()
+    env.setdefault("CONDA_QUIET", "1")
+    return subprocess.run(cmd, check=True, env=env)
 
 
 def test_limited_api_pep725(tmp_path, venv, package_limited_api_pep725):
     _install_external(package_limited_api_pep725)
-    assert Path(sys.prefix, "bin/clang").is_file()
+    assert Path(CONDA_PREFIX, "bin/clang").is_file()
 
     with in_git_repo_context():
         wheel_path = tmp_path / mesonpy.build_wheel(tmp_path)
@@ -33,7 +39,7 @@ def test_limited_api_pep725(tmp_path, venv, package_limited_api_pep725):
 
 # def test_link_against_local_lib_pep725(tmp_path, venv, package_link_against_local_lib_pep725):
 #     _install_external(package_link_against_local_lib_pep725)
-#     assert Path(sys.prefix, "bin/clang").is_file()
+#     assert Path(CONDA_PREFIX, "bin/clang").is_file()
 
 #     with in_git_repo_context():
 #         wheel_path = tmp_path / mesonpy.build_wheel(tmp_path)
