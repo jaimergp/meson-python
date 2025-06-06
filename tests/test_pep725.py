ee080f2a28bcc34a078462e08413fa1a1664ad41
jaimergp <jaimergp@users.noreply.github.com>
2025-06-06 14:05:03 +0200

    better test

--- tests/test_pep725.py
+++ tests/test_pep725.py
@@ -1,52 +1,69 @@
 # SPDX-FileCopyrightText: 2025 The meson-python developers
 #
 # SPDX-License-Identifier: MIT
+import shutil
 import subprocess
 import sys
 
 from pathlib import Path
 
 from pyproject_external import External
+from pytest import CaptureFixture
 
 import mesonpy
+from tests.conftest import CondaEnv
 
 from .conftest import in_git_repo_context
 
 
-def _assert_c_compiler(prefix):
-    if sys.platform.startswith("linux"):
-        assert Path(prefix, "bin/gcc").is_file()
-    elif sys.platform == "darwin":
-        assert Path(prefix, "bin/clang").is_file()
-    elif sys.platform == "win32":
-        pass  # compiler must be present in system separately
-
-
-def _install_external(env_dir: Path, directory: Path, ecosystem: str = "conda-forge") -> subprocess.CompletedProcess:
+def _install_external(
+    env_dir: Path,
+    directory: Path,
+    ecosystem: str = "conda-forge",
+) -> subprocess.CompletedProcess:
     external = External.from_pyproject_path(directory / "pyproject.toml")
     cmd = external.install_command(ecosystem=ecosystem, package_manager="micromamba")
     cmd.append(f"--prefix={env_dir}")
     return subprocess.run(cmd, check=True)
 
 
-def test_limited_api_pep725(tmp_path, conda_env, package_limited_api_pep725):
+def test_limited_api_pep725(
+    tmp_path: Path,
+    conda_env: CondaEnv,
+    package_limited_api_pep725,
+    capfd: CaptureFixture[str],
+):
     _install_external(conda_env, package_limited_api_pep725)
-    _assert_c_compiler(str(conda_env))
+    pkg_config = shutil.which("pkg-config")
+    assert pkg_config
+    assert str(conda_env) in pkg_config
 
     with in_git_repo_context():
         wheel_path = tmp_path / mesonpy.build_wheel(tmp_path)
-    conda_env.pip("install", wheel_path)
+    out, err = capfd.readouterr()
+    assert pkg_config in out + err
 
+    conda_env.pip("install", wheel_path)
     output = conda_env.python("-c", "import module; print(module.add(1, 2))")
     assert int(output) == 3
 
 
-def test_link_against_local_lib_pep725(tmp_path, conda_env, package_link_against_local_lib_pep725):
+def test_link_against_local_lib_pep725(
+    tmp_path: Path,
+    conda_env: CondaEnv,
+    package_link_against_local_lib_pep725,
+    capfd: CaptureFixture[str],
+):
     _install_external(conda_env, package_link_against_local_lib_pep725)
-    _assert_c_compiler(str(conda_env))
+    pkg_config = shutil.which("pkg-config")
+    assert pkg_config
+    assert str(conda_env) in pkg_config
 
     with in_git_repo_context():
         wheel_path = tmp_path / mesonpy.build_wheel(tmp_path)
+    out, err = capfd.readouterr()
+    assert pkg_config in out + err
+
     conda_env.pip("install", wheel_path, "-vvv")
 
     output = conda_env.python("-c", "import example; print(example.example_sum(1, 2))")
